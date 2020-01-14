6b416a69be263e35f763a4f071e7cef239a088b7
GerarST <gerarstanley@gmail.com>
2020-01-14 12:46:13 -0600

    Add: ClassWorks

--- Laboratorio 2/Ejer5.cpp
+++ Laboratorio 2/Ejer5.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct Enterprise{
+    string name;
+    int employees;
+};
+
+int main(){
+    Enterprise* array;
+    int size;
+
+    cout << "Digite cantidad de empersas: "; cin >> size;
+
+    array = new Enterprise[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Nombre:\t"; cin >> array[i].name;
+        cout << "Empleados:\t"; cin >> array[i].employees;
+    }
+    
+
+    return 0;
+}
